db7e33c5b146df2c156909d650b63072df75a3d3
shreyaa <shreyaa.sanjay@gmail.com>
2026-01-31 13:35:56 -0500

    updated setup.py

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -24,8 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'talker = py_pubsub.publisher_member_function:main',
-            'listener = py_pubsub.subscriber_member_function:main',
+            'talker = py_pubsub.publisher_spi:main',
+            'listener = py_pubsub.subscriber_spi:main',
         ],
     },
 )
